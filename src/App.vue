07b00d93b34c2f6e23f52a14c20be02a689e3e05
Jose Campos <josecamposh95@gmail.com>
2020-12-13 14:57:12 -0300

    lifecycle

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Lifecycle />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import Lifecycle from "./components/Lifecycle.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    // HelloWorld,
+    Lifecycle,
   },
 };
 </script>
---
+++ src/components/Lifecycle.vue
@@ -0,0 +1,78 @@
+<template>
+  <section>
+    <h1>Ciclo de Vida - Instancia Vue</h1>
+    <h3 v-for="step in steps" :key="step">{{ step }}</h3>
+    <button @click="destruir()">Destruir Instancia</button>
+    <hr />
+    <h3>Counter: {{ counter }}</h3>
+    <button @click="counter--">-</button>
+    <button @click="counter++">+</button>
+    <div v-html="innnerHTMLCounter"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      steps: [],
+      counter: 0,
+      innnerHTMLCounter: "",
+    };
+  },
+  beforeCreate() {
+    //esto se ejecuta ante o el momento que se crea vue
+    console.log("beforeCreate");
+    this.steps.push("beforeCreate");
+    // this.alert();
+  },
+  created() {
+    //al crear los metodos y observadores de eventos, pero aún no accede al DOM.
+    //Aun no se puede acceder a 'el'
+    this.steps.push("created");
+  },
+  beforeMount() {
+    //se crearan los métodos observadores y eventos, pero aún no accede al DOM.
+    //Aún no se puede acceder al 'el'
+    this.steps.push("beforeMount");
+  },
+  mounted() {
+    //se ejecuta al insertar al dom lee los observadores
+    this.steps.push("mounted");
+  },
+  beforeUpdate() {
+    //al detecatar los cambios
+    console.log("beforeUpdate");
+  },
+  updated() {
+    //al realizar los cambios
+    console.log("updated");
+  },
+  beforeDestroy() {
+    //antes de destruir la instacia
+    this.steps.push("beforeDestroy");
+  },
+  destroyed() {
+    //se destruye toda la instacia
+    this.steps.push("destroyed");
+  },
+  watch: {
+    counter(newCounter, oldCounter) {
+      this.innnerHTMLCounter = `
+        <li>El valor anterior del counter es: ${oldCounter}</li>
+        <li>El nuevo valor del counter es: ${newCounter}</li>
+      `;
+      console.log(`El valor anterior del counter es: ${oldCounter}`);
+      console.log(`El nuevo valor del counter es: ${newCounter}`);
+    },
+  },
+  methods: {
+    destruir() {
+      this.$destroy();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
