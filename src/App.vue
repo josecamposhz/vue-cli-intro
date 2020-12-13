08a69532ba9033301ac66657388f43cf52332b0f
Jose Campos <josecamposh95@gmail.com>
2020-12-13 15:20:48 -0300

    vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Lifecycle />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/lifecycle">Lifecycle</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-// import HelloWorld from "./components/HelloWorld.vue";
-import Lifecycle from "./components/Lifecycle.vue";
-
-export default {
-  name: "App",
-  components: {
-    // HelloWorld,
-    Lifecycle,
-  },
-};
-</script>
 
 <style>
 #app {
@@ -26,6 +16,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
